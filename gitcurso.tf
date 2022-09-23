947a008405279a96e4384c826b6eee03721e887b
ricardoccosta-devops <ccosta.ricardo@gmail.com>
2022-09-23 20:02:41 -0300

    teste

---
+++ gitcurso.tf
@@ -0,0 +1,22 @@
+Git Curso
+
+Comandos
+
+git init
+git config --global user.name ""
+git config --global user.email ""
+
+git status
+
+git add nome do arquivo
+git add . 
+
+commit de arquivos e Projetos
+git commit name do arq -m "descritivo do commit"
+git commmit -m "descritivo do commit"
+
+commit no github da CLOUD 
+git branch -M master
+git remote add origin https://github.com/ricardoccosta-devops/dev.git
+
+git push -u origin master
